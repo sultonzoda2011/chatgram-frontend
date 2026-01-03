c308c2bac709bd2b6ea3f839659d751e79b543b4
Abdulloh <sultonzoda2011@gmail.com>
2026-01-03 19:46:59 +0500

    feat: Implement user authentication with login and registration pages, i18n support, and related UI components.

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import './i18n/i18n.ts'
 import './index.css'
 import App from './App.tsx'
 import Providers from './lib/providers/providers.tsx'
+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
