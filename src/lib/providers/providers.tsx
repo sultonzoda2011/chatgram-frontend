27cfe06c1b568220cc730995aa6d34067488b2f4
Abdulloh <sultonzoda2011@gmail.com>
2026-01-04 13:54:57 +0500

    feat: add login and registration pages with authentication types, schemas, and global providers

--- src/lib/providers/providers.tsx
+++ src/lib/providers/providers.tsx
@@ -2,17 +2,14 @@ import type { ReactNode } from 'react'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { ThemeProvider } from './theme-provider'
 import { Toaster } from 'sonner'
-import { WebSocketProvider } from './websocket-provider'
 const queryClient = new QueryClient()
 
 const Providers = ({ children }: { children: ReactNode }) => {
   return (
     <QueryClientProvider client={queryClient}>
       <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-        <WebSocketProvider>
-          <Toaster closeButton richColors />
-          {children}
-        </WebSocketProvider>
+        <Toaster closeButton richColors />
+        {children}
       </ThemeProvider>
     </QueryClientProvider>
   )
