8bf8970e6f788fc5412e21a0f5be70c51267aa9b
Abdulloh <sultonzoda2011@gmail.com>
2026-01-04 13:55:23 +0500

    feat: Add Zod schemas and TypeScript interfaces for authentication.

--- src/schemas/auth.ts
+++ src/schemas/auth.ts
@@ -1,6 +1,6 @@
 import { z } from 'zod'
 export const LoginSchema = z.object({
-  username: z.string().min(3, 'Username must be at least 3 characters long'),
+  email: z.string().email(),
   password: z.string().min(6, 'Password must be at least 6 characters long'),
 })
 
@@ -8,11 +8,4 @@ export const RegisterSchema = z.object({
   username: z.string().min(3, 'Username must be at least 3 characters long'),
   email: z.string().email(),
   password: z.string().min(6, 'Password must be at least 6 characters long'),
-  confirmPassword: z.string().min(6, 'Password must be at least 6 characters long'),
-})
-
-export const ChangePasswordSchema = z.object({
-  currentPassword: z.string().min(6, 'Current password must be at least 6 characters long'),
-  newPassword: z.string().min(6, 'New password must be at least 6 characters long'),
-  confirmNewPassword: z.string().min(6, 'Confirm new password must be at least 6 characters long'),
 })
