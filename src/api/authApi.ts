bd15a8abbfc49a0fbdd7f94b435e4463c89dc484
Abdulloh <sultonzoda2011@gmail.com>
2026-01-04 10:27:52 +0500

    feat: Implement initial authentication, WebSocket communication, and chat UI components.

--- src/api/authApi.ts
+++ src/api/authApi.ts
@@ -2,18 +2,11 @@ import { api } from '../lib/utils/axiosConfig'
 import type { Login, Register } from '../types/auth'
 
 export const loginApi = async (data: Login) => {
-  try {
-    const response = await api.post('/auth/login', data)
-    return response.data
-  } catch (error) {
-    throw error
-  }
+  const response = await api.post('/auth/login', data)
+  return response.data
 }
+
 export const registerApi = async (data: Register) => {
-  try {
-    const response = await api.post('/auth/register', data)
-    return response.data
-  } catch (error) {
-    throw error
-  }
+  const response = await api.post('/auth/register', data)
+  return response.data
 }
