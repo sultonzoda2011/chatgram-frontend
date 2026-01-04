f1e512bf3ec92b9b7b550162601b83926408e1d1
Abdulloh <sultonzoda2011@gmail.com>
2026-01-04 13:53:30 +0500

    feat: add user authentication with login, registration, API, and i18n support

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,8 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Layout from './components/layout/layout'
-import Home from './pages/main/home/page'
 import { getToken } from './lib/utils/cookie'
 import Login from './pages/auth/login/page'
 import Register from './pages/auth/register/page'
-import Chat from './pages/main/chat/page'
 
 const App = () => {
   const token = getToken()
@@ -13,8 +11,6 @@ const App = () => {
       <Routes>
         {token && (
           <Route path="/" element={<Layout />}>
-            <Route index element={<Home />} />
-            <Route path="/chat/:id" element={<Chat />} />
           </Route>
         )}
         {!token && (
