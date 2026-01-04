ce2488df8e18160b178462a8415a1f9e110177fb
Abdulloh <sultonzoda2011@gmail.com>
2026-01-04 13:54:35 +0500

    feat: Implement initial authentication pages (login, register) with i18n, layout, and global providers.

--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -1,6 +1,5 @@
 import { Navigate, Outlet } from 'react-router-dom'
 import { getToken } from '../../lib/utils/cookie'
-import Sidebar from './sidebar/sidebar'
 
 const Layout = () => {
   const token = getToken()
@@ -9,7 +8,6 @@ const Layout = () => {
   }
   return (
     <div className='flex h-screen w-full overflow-hidden bg-background'>
-      <Sidebar />
       <main className='flex-1 min-w-0 relative flex flex-col'>
         <Outlet />
       </main>
