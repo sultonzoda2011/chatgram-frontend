67f3531aa620a84379c644ddfeb224b894f4abf5
Abdulloh <sultonzoda2011@gmail.com>
2026-01-04 10:28:09 +0500

    feat: Introduce core chat application frontend including authentication, contacts, messaging, and real-time communication.

--- src/api/contactsApi.ts
+++ src/api/contactsApi.ts
@@ -2,10 +2,6 @@ import { api } from '../lib/utils/axiosConfig'
 import type { Contact } from '../types/contacts'
 
 export const getContacts = async (): Promise<Contact[]> => {
-  try {
-    const response = await api.get('/contacts')
-    return response.data.data
-  } catch (error) {
-    throw error
-  }
+  const response = await api.get('/contacts')
+  return response.data.data
 }
