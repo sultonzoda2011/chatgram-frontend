7bd997115a5e5ef5c66aa6db25d134807fc67876
Abdulloh <sultonzoda2011@gmail.com>
2026-01-04 10:34:19 +0500

    feat: introduce core type definitions for application entities and configure WebSocket environment variable.

--- src/types/auth.ts
+++ src/types/auth.ts
@@ -1,17 +1,23 @@
-export interface ILogin {
+export interface Login {
   username: string
   password: string
 }
 
-export interface IRegister {
+export interface Register {
   username: string
   email: string
   password: string
   confirmPassword: string
 }
 
-export interface IChangePassword {
+export interface ChangePassword {
   currentPassword: string
   newPassword: string
   confirmNewPassword: string
+}
+
+export interface JwtPayload {
+  userId: string
+  iat: number
+  exp: number
 }
