177efb042d0168f0b95aae13bef895a1703d2d80
Abdulloh <sultonzoda2011@gmail.com>
2026-01-04 10:21:39 +0500

    feat: wrap Providers with WebSocketProvider to enable real-time communication support alongside query and theme providers

--- src/lib/providers/providers.tsx
+++ src/lib/providers/providers.tsx
@@ -2,14 +2,17 @@ import type { ReactNode } from 'react'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { ThemeProvider } from './theme-provider'
 import { Toaster } from 'sonner'
+import { WebSocketProvider } from './websocket-provider'
 const queryClient = new QueryClient()
 
 const Providers = ({ children }: { children: ReactNode }) => {
   return (
     <QueryClientProvider client={queryClient}>
       <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-        <Toaster closeButton richColors />
-        {children}
+        <WebSocketProvider>
+          <Toaster closeButton richColors />
+          {children}
+        </WebSocketProvider>
       </ThemeProvider>
     </QueryClientProvider>
   )
