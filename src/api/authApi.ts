43463443500f4b7f43c876af8bc71a9774fabc8b
Abdulloh <sultonzoda2011@gmail.com>
2026-01-04 13:53:41 +0500

    feat: Add complete authentication flow including login, registration, layout, API, and i18n.

--- src/api/authApi.ts
+++ src/api/authApi.ts
@@ -1,12 +1,12 @@
 import { api } from '../lib/utils/axiosConfig'
-import type { Login, Register } from '../types/auth'
+import type { ILogin, IRegister } from '../types/auth'
 
-export const loginApi = async (data: Login) => {
+export const loginApi = async (data: ILogin) => {
   const response = await api.post('/auth/login', data)
   return response.data
 }
 
-export const registerApi = async (data: Register) => {
+export const registerApi = async (data: IRegister) => {
   const response = await api.post('/auth/register', data)
   return response.data
 }
