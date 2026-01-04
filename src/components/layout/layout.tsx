5e7082a712b958e625409a3bf30278e14d00959a
Abdulloh <sultonzoda2011@gmail.com>
2026-01-04 07:29:55 +0500

    feat: Add initial application layout, authentication pages, and main content pages with contact types.

--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -1,13 +1,18 @@
 import { Navigate, Outlet } from 'react-router-dom'
 import { getToken } from '../../lib/utils/cookie'
+import Sidebar from './sidebar/sidebar'
+
 const Layout = () => {
   const token = getToken()
   if (!token) {
     return <Navigate to="/login" />
   }
   return (
-    <div >
-      <Outlet />
+    <div className='flex'>
+      <Sidebar />
+      <div className='w-full'>
+        <Outlet />
+      </div>
     </div>
   )
 }
