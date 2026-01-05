da8319530f1de87f024385c3cbd8d08c59a98ca7
Abdulloh <sultonzoda2011@gmail.com>
2026-01-05 09:06:47 +0500

    feat: Add authentication pages (login, register), i18n support, and core layout components.

--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -1,5 +1,6 @@
 import { Navigate, Outlet } from 'react-router-dom'
 import { getToken } from '../../lib/utils/cookie'
+import Sidebar from './sidebar'
 
 const Layout = () => {
   const token = getToken()
@@ -8,6 +9,7 @@ const Layout = () => {
   }
   return (
     <div className='flex h-screen w-full overflow-hidden bg-background'>
+      <Sidebar />
       <main className='flex-1 min-w-0 relative flex flex-col'>
         <Outlet />
       </main>
