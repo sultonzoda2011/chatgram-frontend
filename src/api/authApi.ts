a2a874496d76b0701088fb40ad13e129e70a5c32
Abdulloh <sultonzoda2011@gmail.com>
2026-01-05 16:15:45 +0500

    feat: add initial frontend setup including core pages, UI components, API services, and type definitions.

--- src/api/authApi.ts
+++ src/api/authApi.ts
@@ -1,5 +1,5 @@
 import { api } from '../lib/utils/axiosConfig'
-import type { ILogin, IRegister } from '../types/auth'
+import type { IChangePassword, ILogin, IRegister, IUpdateProfile } from '../types/auth'
 
 export const loginApi = async (data: ILogin) => {
   const response = await api.post('/auth/login', data)
@@ -8,5 +8,19 @@ export const loginApi = async (data: ILogin) => {
 
 export const registerApi = async (data: IRegister) => {
   const response = await api.post('/auth/register', data)
+  return response.data
+}
+
+export const getProfile = async () => {
+  const response = await api.get('/auth/profile')
+  return response.data
+}
+
+export const updateProfile = async (data: IUpdateProfile) => {
+  const response = await api.post('/auth/profile/update', data)
+  return response.data
+}
+export const changePassword = async (data: IChangePassword) => {
+  const response = await api.post('/auth/profile/change-password', data)
   return response.data
 }
