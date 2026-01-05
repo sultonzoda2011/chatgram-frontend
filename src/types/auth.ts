82c66e067735bbfefeecdce4d7ddeb93c15f0cb0
Abdulloh <sultonzoda2011@gmail.com>
2026-01-05 09:07:45 +0500

    feat: Add authentication interfaces for login, registration, profile, and JWT payload.

--- src/types/auth.ts
+++ src/types/auth.ts
@@ -1,16 +1,32 @@
 export interface ILogin {
-  email: string
+  username: string
   password: string
 }
 
 export interface IRegister {
   username: string
+  fullname: string
   email: string
   password: string
 }
-
-
-
+export interface IProfile {
+  id: number
+  username: string
+  fullname: string
+  email: string
+  avatar: string
+}
+export interface IUpdateProfile {
+  username: string
+  fullname: string
+  email: string
+  avatar: File
+}
+export interface IChangePassword {
+  oldPassword: string
+  newPassword: string
+  confirmPassword: string
+}
 export interface JwtPayload {
   userId: string
   iat: number
