ffd39c1379a17b0867bfd357cf3ffd38a326fb4e
Abdulloh <sultonzoda2011@gmail.com>
2026-01-05 16:18:43 +0500

    feat: Introduce type definitions for authentication, chat, and user data, and authentication schemas.

--- src/schemas/auth.ts
+++ src/schemas/auth.ts
@@ -12,19 +12,11 @@ export const RegisterSchema = z.object({
   password: z.string().min(1, 'Пароль обязателен').min(6, 'Минимум 6 символов'),
 })
 
-export const ProfileSchema = z.object({
-  id: z.number({}).int(),
-  username: z.string().min(1, 'Имя пользователя обязательно'),
-  fullname: z.string().min(1, 'Полное имя обязательно'),
-  email: z.string().min(1, 'Email обязателен').email('Неверный email'),
-  avatar: z.string().url('Неверный URL аватара').nullable().optional(),
-})
 
 export const UpdateProfileSchema = z.object({
   username: z.string().min(1, 'Имя пользователя обязательно').min(3, 'Минимум 3 символа'),
   fullname: z.string().min(1, 'Полное имя обязательно').min(3, 'Минимум 3 символа'),
   email: z.string().min(1, 'Email обязателен').email('Неверный email'),
-  avatar: z.instanceof(File, { message: 'Файл должен быть изображением' }).optional(),
 })
 
 export const ChangePasswordSchema = z.object({
