ace53652e0a38674c85b1ba15200c3e28c68e5b3
Abdulloh <sultonzoda2011@gmail.com>
2026-01-05 16:42:05 +0500

    feat: Add new pages for home, chat, profile, and not-found along with foundational UI components and utilities.

--- src/components/ui/label.tsx
+++ src/components/ui/label.tsx
@@ -1,6 +1,6 @@
 import * as React from "react"
 import * as LabelPrimitive from "@radix-ui/react-label"
-import { cn } from '../../lib/utils/shadUtils'
+import { cn } from "../../lib/utils/cn"
 
 
 function Label({
