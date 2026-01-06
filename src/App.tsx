a176b076e1efab577b87b0cb86b4bb6b9ef149db
Abdulloh <sultonzoda2011@gmail.com>
2026-01-06 08:24:39 +0500

    feat: scaffold initial chat application frontend including core pages, components, and internationalization.

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,33 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import Layout from './components/layout/layout'
-import { getToken } from './lib/utils/cookie'
 import Login from './pages/auth/login/page'
 import Register from './pages/auth/register/page'
 import Chat from './pages/main/chat/page'
 import Profile from './pages/main/profile/page'
 import NotFound from './pages/not-found/page'
 import Home from './pages/main/home/page'
+import Layout from './components/layout/layout'
+import PrivateRoute from './components/layout/privateRoute'
 
 const App = () => {
-  const token = getToken()
   return (
     <BrowserRouter>
       <Routes>
-        {token && (
-          <Route path="/" element={<Layout />}>
-            <Route index element={<Home />} />
-            <Route path="*" element={<NotFound />} />
-            <Route path="chat/:id" element={<Chat />} />
-            <Route path="profile" element={<Profile />} />
-          </Route>
-        )}
-        {!token && (
-          <>
-            <Route path="/login" element={<Login />} />
-            <Route path="/register" element={<Register />} />
-          </>
-        )}
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+
+        <Route
+          path="/"
+          element={
+            <PrivateRoute>
+              <Layout />
+            </PrivateRoute>
+          }
+        >
+          <Route index element={<Home />} />
+          <Route path="chat/:id" element={<Chat />} />
+          <Route path="profile" element={<Profile />} />
+          <Route path="*" element={<NotFound />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   )
