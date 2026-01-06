b94eabae4047bc3feb27f30f8f7569424d6f4b73
Abdulloh <sultonzoda2011@gmail.com>
2026-01-06 19:38:32 +0500

    feat:androind settings

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.chatgram.app',
+  appName: 'ChatGram',
+  webDir: 'dist'
+};
+
+export default config;
