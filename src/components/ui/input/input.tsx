325824fc24a68536428a96a57f42a14dc5f96f8a
Abdulloh <sultonzoda2011@gmail.com>
2026-01-06 08:29:59 +0500

    feat: Add core frontend pages, authentication, profile management, and internationalization support.

--- src/components/ui/input/input.tsx
+++ src/components/ui/input/input.tsx
@@ -2,6 +2,7 @@ import * as React from "react"
 import { cn } from "../../../lib/utils/cn"
 
 function Input({ className, type, ...props }: React.ComponentProps<"input">) {
+
   return (
     <input
       type={type}
